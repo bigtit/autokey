34bbaaa44f6d0eda8d0af4de6b66c08f4beb47b7
bigtit <bigtit@yeah.net>
2014-02-02 22:08:58 +0800

    Update hk.c

--- hk/hk.c
+++ hk/hk.c
@@ -1,5 +1,5 @@
 // hk.c
-// 3.0 version
+// 3.1 version
 // use low level keybd hook
 // to avoid keybd_event confusion with keybd hook
 #include <windows.h>
@@ -18,7 +18,7 @@ HWND dlg = 0;         // save dlg handler
 // no shared
 HHOOK hook = 0;       // main hook
 HINSTANCE inst = 0;   // dll instance
-HWND wnd = 0;         // target wnd
+// HWND wnd = 0;         // target wnd
 BOOL ton = 0;         // is timer on
 UINT_PTR tid = 0;     // timer id
 
@@ -27,21 +27,22 @@ LRESULT CALLBACK tproc(HWND, UINT, UINT, DWORD);
 
 BOOL APIENTRY DllMain(HINSTANCE hDll, DWORD dwReason, LPVOID lpReserved)
 {
-  switch(dwReason){
+	switch(dwReason){
     case DLL_PROCESS_ATTACH:
       inst = hDll;
       DisableThreadLibraryCalls(inst);
       break;
     case DLL_PROCESS_DETACH:
       break;
-    case DLL_THREAD_ATTACH: break;
-    case DLL_THREAD_DETACH: break;
-  }
-  return TRUE;
+		case DLL_THREAD_ATTACH: break;
+		case DLL_THREAD_DETACH: break;
+	}
+	return TRUE;
 }
 BOOL WINAPI __stdcall SetDlg(HWND hdlg)
 {
   dlg = hdlg;
+
   return 0;
 }
 BOOL WINAPI __stdcall SetSkey(char skey)
@@ -49,7 +50,6 @@ BOOL WINAPI __stdcall SetSkey(char skey)
   sk = skey;
   return 0;
 }
-
 BOOL WINAPI __stdcall SetHook(int mode, int iterval, char key)
 {
   //int lerr;
@@ -63,7 +63,7 @@ BOOL WINAPI __stdcall SetHook(int mode, int iterval, char key)
   ton = 0;
 
   if(md){
-    if(wnd) return 0;
+    // if(wnd) return 0;
     hook = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC)llkproc, inst, 0);
     ok=(hook!=0);
   }
@@ -72,7 +72,7 @@ BOOL WINAPI __stdcall SetHook(int mode, int iterval, char key)
     if(ok){
       KillTimer(0, tid);
       hook = 0;
-      wnd = 0;
+      // wnd = 0;
     }
   }
   return ok;
@@ -84,13 +84,14 @@ LRESULT CALLBACK llkproc(int code, WPARAM wp, LPARAM lp)
   BOOL kup = WM_KEYUP==wp || WM_SYSKEYUP==wp;
   BOOL kdn = WM_KEYDOWN==wp || WM_SYSKEYDOWN==wp;
   GetCursorPos(&pt);
-  // wnd = GetActiveWindow();
-  wnd = WindowFromPoint(pt);
+
+  // wnd = WindowFromPoint(pt);
+  // wnd = FindWindow("KGWin32App", "剑侠情缘网络版叁");
+  // if(!wnd) return -1;
 
   if(code!=HC_ACTION)
     return CallNextHookEx(hook, code, wp, lp);
   
-  // no problem with on/off mode(mode 1)
   if(md==1 && kup && ks->vkCode==sk){
     if(!ton){
       keybd_event(ky, 0, 0, 0);
@@ -106,7 +107,7 @@ LRESULT CALLBACK llkproc(int code, WPARAM wp, LPARAM lp)
     }
     return 1;
   }
-  else if(md==2 && ks->scanCode==MapVirtualKey(ky, MAPVK_VK_TO_VSC)){
+  else if(md==1 && ks->scanCode==MapVirtualKey(ky, MAPVK_VK_TO_VSC)){
     if(!ton && kdn){
       keybd_event(ky, 0, 0, 0);
       keybd_event(ky, 0, KEYEVENTF_KEYUP, 0);
@@ -129,20 +130,5 @@ LRESULT CALLBACK tproc(HWND hwnd, UINT msg, UINT id, DWORD time)
   // 0 in 3rd parameter means KEYEVENTF_KEYDOWN
   keybd_event(ky, 0, 0, 0);
   keybd_event(ky, 0, KEYEVENTF_KEYUP, 0);
-  // PostMessage is only availible in text area of jx3;
-  // PostMessage(wnd, WM_KEYDOWN, ky, llp);
-  // PostMessage(wnd, WM_KEYUP, ky, llp);
-  /*
-  INPUT inp[2];
-  memset(inp, 0, sizeof(inp));
-  inp[0].type = INPUT_KEYBOARD;
-  inp[0].ki.wVk = ky;
-  inp[0].ki.wScan = MapVirtualKey(ky, MAPVK_VK_TO_VSC);
-  inp[1].type = inp[0].type;
-  inp[1].ki.wVk = ky;
-  inp[1].ki.wScan = inp[0].ki.wScan;
-  inp[1].ki.dwFlags = KEYEVENTF_KEYUP;
-  SendInput(2, inp, sizeof(INPUT));
-  */
   return 1;
 }
